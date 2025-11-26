3b0c4a7b45bc6e87e219d85056d16e05ae0f4992
premalatha <169128822+premalatha7647@users.noreply.github.com>
2025-11-26 08:59:31 +0530

    pattern

--- pattern.js
+++ pattern.js
@@ -1,6 +1,101 @@
-let n = 4;
+let n = 5;
+for (let i = 0; i < n; i++) {
+  let row = "";
+  for (let j = 0; j < n; j++) {
+    row += "* ";
+  }
+  console.log(row);
+}
+console.log("________________________");
+for (let i = 0; i < n; i++) {
+  let row = "";
+  for (let j = 0; j < i; j++) {
+    row += "* ";
+  }
+  console.log(row);
+}
+console.log("________________________");
+for (let i = 0; i < n; i++) {
+  let row = "";
+  for (k = 0; k < n - i; k++) {
+    row += " ";
+  }
+  for (let j = 0; j <= i; j++) {
+    row += "* ";
+  }
+  console.log(row);
+}
+console.log("________________________");
+/* 
+1
+12
+123
+1234
+12345
+*/
+for (let i = 1; i <= n; i++) {
+  let row = "";
+  for (let j = 1; j <= i; j++) {
+    row += j;
+  }
+  console.log(row);
+}
+console.log("________________________");
+/*
+1
+22
+333
+4444
+55555
+*/
 
+for (let i = 0; i < n; i++) {
+  let row = "";
+  for (let j = 0; j <= i; j++) {
+    row += i + 1;
+  }
+  console.log(row);
+}
 
+console.log("________________________");
+for (let i = 0; i < n; i++) {
+  let row = "";
+  for (let j = 0; j < n - i; j++) {
+    row += j + 1;
+  }
+  console.log(row);
+}
+console.log("________________________");
+for (let i = 0; i < n; i++) {
+  let row = "";
+  for (let j = 0; j < n - i; j++) {
+    row += "*";
+  }
+  console.log(row);
+}
+console.log("________________________");
+
+for (let i = 0; i < n; i++) {
+  let row = "";
+  for ( let j=0;j <= n - i - 1; j++) {
+    row += " ";
+  }
+  for (let k = 0; k <= i; k++) {
+    row += "*";
+  }
+  console.log(row);
+}
+
+console.log("________________________");
+
+for(let i=1; i<=n;i++){
+   let row = "";
+  for(let j=1; j<=i; j++){
+      row += j%2
+  }
+    console.log(row);
+}
+console.log("________________________");
 
 for (i = 1; i <= 5; i++) {
   let row = "";
@@ -9,9 +104,7 @@ for (i = 1; i <= 5; i++) {
   }
   console.log(row);
 }
-
-n = 4;
-
+console.log("________________________");
 for (i = 0; i < n; i++) {
   let row = "";
   for (k = 0; k < n - i; k++) {
@@ -22,7 +115,7 @@ for (i = 0; i < n; i++) {
   }
   console.log(row);
 }
-
+console.log("________________________");
 for (i = 0; i < n; i++) {
   let row = "";
   for (k = 0; k < n - i; k++) {
@@ -33,4 +126,3 @@ for (i = 0; i < n; i++) {
   }
   console.log(row);
 }
-
